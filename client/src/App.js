196b5427b9d394792bba4941aed261cf7f8fcef3
Andrew <aiglinski@icloud.com>
2019-05-16 12:13:35 -0500

    working as of installation and application of bootstrap, 14:00 on vid 3

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi there fella</h1>
+    </div>
+  );
+}
+
+export default App;
