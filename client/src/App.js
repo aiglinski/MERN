5d4b40319d772ef253f64f6054d5b6e8ccc3968f
Andrew <aiglinski@icloud.com>
2019-05-18 12:31:53 -0500

    redux started, but need to finish. Backing up as it is functional as of the end of vid 5

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import ShoppingList from "./components/ShoppingList";
 
+import { Provider } from "react-redux";
+
+import store from "./components/store";
+
 function App() {
   return (
-    <div className="App">
-      <AppNavbar />
-      <ShoppingList />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar />
+        <ShoppingList />
+      </div>
+    </Provider>
   );
 }
 
